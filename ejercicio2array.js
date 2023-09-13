12cdefaa79b03d4eb007be4acb8031d500f937e2
Misael <betancourthmisael2001@gmail.com>
2023-09-12 20:18:22 -0600

    Agregando comentarios al codigo

--- ejercicio2array.js
+++ ejercicio2array.js
@@ -1,12 +1,12 @@
 let myString = 'Javascript+es+super+cool'
 
-let miarray=myString.split('+')
+let miarray=myString.split('+') //Cambiando el + por , entre elementos y convirtiendo en array
 console.log(miarray)
-let Arraylength= miarray.length
-console.log(Arraylength)
-let lastItem =(miarray[miarray.length-1])
+let Arraylength= miarray.length  //Mostrando el total del array 
+console.log(Arraylength) 
+let lastItem =(miarray[miarray.length-1]) //Mostrando el ultimo elemento del array
 console.log(lastItem)
-myInverseArray= miarray.reverse()
-console.log(myInverseArray)
-myInverseArray.push('Gusta')
+myInverseArray= miarray.reverse() //Invirtiendo los elementos del array
+console.log(myInverseArray) 
+myInverseArray.push('Gusta') //Agregando un elemento al final del arrays
 console.log(myInverseArray)
