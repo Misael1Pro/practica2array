242003802338d470838cb2aa61f1dd892495cacf
Misael <betancourthmisael2001@gmail.com>
2023-09-12 20:14:59 -0600

    Subiendo practica2 de array

---
+++ ejercicio2array.js
@@ -0,0 +1,12 @@
+let myString = 'Javascript+es+super+cool'
+
+let miarray=myString.split('+')
+console.log(miarray)
+let Arraylength= miarray.length
+console.log(Arraylength)
+let lastItem =(miarray[miarray.length-1])
+console.log(lastItem)
+myInverseArray= miarray.reverse()
+console.log(myInverseArray)
+myInverseArray.push('Gusta')
+console.log(myInverseArray)
